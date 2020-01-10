96542ac9bb7e16cfc31b61bfa9476a2f7ce630d2
Andrea Borruso <aborruso@gmail.com>
2020-01-10 10:54:34 +0100

    aggiunto report noindex

--- noIndexPA.sh
+++ noIndexPA.sh
@@ -16,5 +16,26 @@ mlr --n2t --ifs ";" cut -f 7 then filter -x '$7=="" || $7!=~"^htt" || $7=="Link"
   then uniq -a then cat -n \
   then rename 7,URL "$folder"/amministrazionTrasparente.csv | tail -n +2 >"$folder"/bussolaPA/amministrazionTrasparenteURL.tsv
 
-# scarica pagina
+# scarica body e header delle pagine
 parallel --colsep "\t" 'curl --max-time 60 -k -sL {2} -o '"$folder"'/bussolaPA/{1}.txt  -D '"$folder"'/bussolaPA/{1}_code -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "Sec-Fetch-User: ?1" --compressed' :::: "$folder"/bussolaPA/amministrazionTrasparenteURL.tsv
+
+# check noindex
+grep -l -E 'meta.+noindex' "$folder"/bussolaPA/*.txt | grep -oE '[0-9]+' >"$folder"/bussolaPA/alertnoindex.tsv
+mlr -I --tsv --implicit-csv-header label n bussolaPA/alertnoindex.tsv
+
+csvsql -d ";" -I -u=0 --query "select * from amministrazionTrasparente" "$folder"/amministrazionTrasparente.csv >"$folder"/tmp.csv
+cat "$folder"/tmp.csv >"$folder"/amministrazionTrasparente.csv
+
+
+
+mlr --tsv --implicit-csv-header then label n,URL "$folder"/bussolaPA/amministrazionTrasparenteURL.tsv >"$folder"/bussolaPA/tmp_source.tsv
+
+mlr --t2c join --ul -j n -f "$folder"/bussolaPA/alertnoindex.tsv then unsparsify "$folder"/bussolaPA/tmp_source.tsv >"$folder"/bussolaPA/alertnoindexURL.csv
+
+mlr --csv join --ul -j Link -l Link -r URL -f "$folder"/amministrazionTrasparente.csv \
+then unsparsify \
+then rename n,noindex \
+then put -S 'if ($noindex=="") {$noindex=""} else {$noindex="x"}' \
+then reorder -e -f Link \
+then sort -f Regione,Provincia,Comune,Denominazione "$folder"/bussolaPA/alertnoindexURL.csv >"$folder"/reportAmministrazionTrasparenteBussola.csv
+
